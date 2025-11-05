793eb86591f38c46d2ed456a35cc8654bc981c6a
Erik Emilov <erik2006de@gmail.com>
2025-11-05 23:36:52 +0200

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,42 @@
+
+
+
+#include <iostream>
+    using namespace std;
+    int main()
+    {
+        double num1, num2;
+        char op;
+
+        cout << "Hello, this is my calculator in C++";
+		
+        cout << "Enter the Operator (+, -, *, /): ";
+        cin >> op;
+        cout << "Enter two numbers: ";
+        cin >> num1 >> num2;
+        switch (op)
+        {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << num1 << " / " << num2 << " = " << num1 / num2;
+            
+            else
+                cout << "Error! Division by zero.";
+            break;
+        default:
+            cout << "Error! Operator is not correct";
+            break;
+        }
+    }
+
+
+
